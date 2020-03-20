73e53c95c28a397a1e91e66c00a6a9a1dc049dd8
Amber Mohler <ambermohler@iMac.local>
2020-03-20 13:05:04 -0400

    finished projects-router  endpoints

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -18,6 +18,29 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  Projects.findById(id).then(project => {
+    if (project) {
+      res.status(200).json(project);
+    } else {
+      res
+        .status(404)
+        .json({ message: "Could not find a project with that ID" });
+    }
+  });
+});
+
+router.post("/", (req, res) => {
+  const newProject = req.body;
+  Projects.add(newProject)
+    .then(project => {
+      res.status(201).json({ message: "Project successfully added" });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error adding project" });
+    });
+});
 
 module.exports = router;
